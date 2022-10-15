eb3175390e8751a8156ad9f137e7767079e3bdb1
Benildus <88126982+Benil2019@users.noreply.github.com>
2022-10-15 10:15:45 +0530

    Add files via upload

---
+++ Assignment/Benildus/Assignment 1/benildus_assignment_1.ino
@@ -0,0 +1,34 @@
+// C++ code
+//
+void setup()
+{
+ pinMode(8,OUTPUT);
+ pinMode(5,OUTPUT);
+ Serial.begin(9600);
+}
+void loop()
+{
+  int a=analogRead(A0);
+  int b=analogRead(A3);
+  if(a>100)
+  {
+    digitalWrite(8,HIGH);
+    Serial.println("motion detected");
+  }
+  else
+  {
+    digitalWrite(8,LOW);
+    Serial.println("motion not detected");
+  } 
+  if(b>300)
+  {
+    digitalWrite(5,HIGH);
+    Serial.println("gas leakage detected");
+  }
+  else
+  {
+    digitalWrite(5,LOW);
+    Serial.println("gas leakage not detected");
+  }
+  delay(1000);
+} 
